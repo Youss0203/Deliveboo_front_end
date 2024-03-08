5fb68b22d310054db14416bfb0b7f63c7615e82a
BernardoBarbucci <bernardobarbucci@gmail.com>
2024-03-08 18:34:40 +0100

    footer inserito

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,7 +1,13 @@
 <template lang="">
-    <div>
-        
-    </div>
+    <nav class="navbar fixed-bottom my_bg">
+        <div class="container-fluid">
+            <a class="navbar-brand fw-bold">Footer</a>
+            <form class="d-flex" role="search">
+                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success" type="submit">Search</button>
+            </form>
+        </div>
+    </nav>
 </template>
 <script>
 export default {
@@ -9,5 +15,7 @@ export default {
 }
 </script>
 <style lang="scss">
-    
+    .my_bg{
+        background-color: #FD7F00;
+    }
 </style>
