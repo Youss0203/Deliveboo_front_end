a127aa612234ded1205c81e0ee4aaab1a0eb4f36
Youss0203 <youssefhalibi003@gmail.com>
2024-03-08 17:55:45 +0100

    inizio nuovo progetto

---
+++ src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<template lang="">
+  <div>
+    <h1>ciao</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style lang="scss" scoped>
+  h1{
+    color:red;
+  }
+</style>
