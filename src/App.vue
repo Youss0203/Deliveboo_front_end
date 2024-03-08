647ebd40e250112ce99317c45ee5a01fe58febbc
Youss0203 <youssefhalibi003@gmail.com>
2024-03-08 18:29:00 +0100

    creato header

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,19 @@
 <template>
+  <AppHeader />
+  <AppFooter />
   <RouterView />
+  
 </template>
 
 
 <script>
+  import AppHeader from './components/AppHeader.vue';
+  import AppFooter from './components/AppFooter.vue';
+
+
 export default {
-  
+
+    components: { AppHeader, AppFooter }
 }
 </script>
 
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,28 @@
+<template lang="">
+     <div>
+        <nav class="navbar  my_bg">
+  <div class="container-fluid ">
+    <a class="navbar-brand " href="#">
+      <img src="" alt="Logo" width="30" height="24" class="d-inline-block align-text-top">
+    </a>
+    <div>
+        <a href="http://127.0.0.1:8000">Area Ristoratore</a>
+    </div>
+  </div>
+</nav>
+    </div>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style lang="scss">
+    .my_bg{
+        background-color: #FD7F00;
+    }
+    a{
+        text-decoration: none;
+        color:black ;
+    }
+</style>
